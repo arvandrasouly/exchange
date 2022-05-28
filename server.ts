4f65efe10c32dbfe33eee51c61055e0c2f9b4a8e
arvand rasouly <arvandrasouly@gmail.com>
2022-05-28 11:52:05 +0430

    Server file updated

--- server.ts
+++ server.ts
@@ -7,22 +7,25 @@ import morgan from "morgan";
 import methodOverride from "method-override";
 import debug from "debug";
 import api from "./api";
+import swagger from "./common/swagger"
 
+// loading environment variables
 dotenv.config()
+
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || '3000'
 
+// initializing express
 const server = express()
 
-// log
+// setup requests logger
 const logger = dev? morgan("dev") : morgan("common")
 server.use(logger)
 
 const appDebug = debug('farm:server')
+// server.use(appDebug)
 
 server.set('port', port)
-
-server.use(appDebug)
 
 const onAppError = (error) => {
   if (error.syscall !== 'listen') {
@@ -49,32 +52,43 @@ const onAppError = (error) => {
 }
 server.on('error', onAppError)
 
+//
 if (!dev) {
   server.set('trust proxy', 1); // sets req.hostname, req.ip
-  //sessionOptions.cookie.secure = true; // sets cookie over HTTPS only
 }
 
+// Initialize swagger in dev mode
+if (dev) {
+  swagger(server)
+}
+// secure app by setting various HTTP headers
 server.use(helmet());
+
+// support CORS for API calls
 server.use(cors({
   origin: dev ? process.env.APP_URL : process.env.PRODUCTION_URL_APP,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }))
+// enable json requests
 server.use(express.json())
+// allow method overrides
 server.use(methodOverride('_method'))
+// enable url encoding
 server.use(express.urlencoded({ extended: true }))
-// const compression = require('compression')
-// app.use(compression)
 
+// Initialize api routes
 api(server)
 
+// Create server
 const httpServer = httpModule.createServer(server);
 
+// handle unhandled routes
 server.get('*', (req, res) => {
   res.status(403).send();
 });
 
+// start listening to port for incoming requests
 httpServer.listen(port, () => {
-  logger.debug('debug right before info');
-  logger.info(`> Ready on ${dev ? process.env.API_URL : process.env.PRODUCTION_URL_API}`);
+    console.log(`> Ready on ${dev ? process.env.API_URL : process.env.PRODUCTION_URL_API}`);
 })
