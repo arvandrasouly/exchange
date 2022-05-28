74f2248b67e32db041f17373e3a3fe6fe23123fd
arvand rasouly <arvandrasouly@gmail.com>
2022-05-28 13:01:06 +0430

    default api route added

--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,10 @@ import express, {Request, Response} from "express"
 
 const router = express.Router()
 
+router.get('/', (req: Request, res: Response) => {
+  res.send('This is apt root')
+})
+
 router.get('/price', (req: Request, res: Response) => {
   res.send('This is price list')
 })
