dff6bd191fab034d118064afd6b47118e7fcd8ce
arvand rasouly <arvandrasouly@gmail.com>
2022-05-28 11:51:01 +0430

    Swagger added

---
+++ common/swagger.ts
@@ -0,0 +1,34 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+function swagger(server) {
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Exchange API with Swagger",
+                version: "0.1.0",
+                description: "API documentation with Swagger",
+                license: {
+                    name: "MIT",
+                    url: "https://spdx.org/licenses/MIT.html",
+                }
+            },
+            servers: [
+                {
+                    url: "http://localhost:3000/api",
+                },
+            ],
+        },
+        apis: ["./doc/swagger/coin.js"],
+    };
+
+    const specs = swaggerJsdoc(options);
+    server.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(specs)
+    );
+}
+
+export default swagger;
